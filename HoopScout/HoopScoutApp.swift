80a4116fe56c2ec7f4430c417bbde37eb1c10714
Christopher Doyle <southpawcoder@icloud.com>
2026-04-21 13:20:28 -0400

    Initial Commit

---
+++ HoopScout/HoopScoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoopScoutApp.swift
+//  HoopScout
+//
+//  Created by Christopher Doyle on 4/21/26.
+//
+
+import SwiftUI
+
+@main
+struct HoopScoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
